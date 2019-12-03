ead75b7b4a0396357f3a064acc0003c89a241972
Genki Watanabe <gitkotobuki@gmail.com>
2019-12-03 19:03:06 +0900

    first commit

---
+++ src/pages/Skill.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './../App.css';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+const Skill = () => (
+  <div>
+    <div><Link to='/'>Index</Link></div>
+    <div><Link to='/profile'>Profile</Link></div>
+    <div><Link to='/skill'>Skill</Link></div>
+    <div><Link to='/works'>Works</Link></div>
+    <div><Link to='/contact'>Contact</Link></div>
+  </div>
+)
+
+export default Skill
